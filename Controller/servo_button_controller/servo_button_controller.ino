3e8170710d92299757d19c66a8d31defc1ce57db
NeilSCGH <neil.segard@gmail.com>
2020-06-08 14:39:35 +0200

    basis for controlling the motor

---
+++ Controller/servo_button_controller/servo_button_controller.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  
+  myservo.write(0);
+  //delay(3000);
+}
+
+void loop() {
+  myservo.write(0);
+  delay(2000);
+  myservo.write(75);
+  delay(2000);
+}
